bbb6919269f3539201ff65ff9dca6c4a3c944b22
Nikola Gazdic <nikola.gazdic90@gmail.com>
2022-07-14 15:53:02 -0400

    completed

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        public BankAccount()
+        {
+        }
+
+        private double balance = 0;
+
+        public void Deposit(double dep)
+        {
+            balance = balance + dep;
+
+        }
+
+
+        public double GetBalance()
+        {
+
+            return balance;
+
+        }
+
+
+    }
+}
+
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,17 @@
+﻿using BankEncapsulation;
+
+
+
+
+Console.WriteLine("Enter an ammount to deposit:");
+var c = new BankAccount();
+
+//var userInput = double.Parse(Console.ReadLine());
+
+//c.Deposit(userInput);
+
+c.Deposit(Convert.ToDouble(Console.ReadLine()));
+
+Console.WriteLine("Your balance is:");
+Console.WriteLine(c.GetBalance());
+
